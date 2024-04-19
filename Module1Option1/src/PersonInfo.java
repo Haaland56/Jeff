c12a0f2baccabe5addb738538e48511f694138ff
Haaland56 <138836413+Haaland56@users.noreply.github.com>
2024-04-19 14:13:57 -0700

    Critical thinking #1

---
+++ Module1Option1/src/PersonInfo.java
@@ -0,0 +1,23 @@
+
+public class PersonInfo {
+
+	public static void main(String[] args) {
+
+		// Define variables
+		String firstName = "Japheth";
+		String lastName = "Ouko";
+		String streetAddress = "55 Idaho Way";
+		String city = "Idaho";
+		int zipCode = 5000;
+
+		// Print personal information
+
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+
+	}
+
+}
